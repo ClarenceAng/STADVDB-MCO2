3b81f331b282578aa5e3e503925cc6370be06c30
ForgedLives <forgedlives@gmail.com>
2025-11-25 00:17:42 +0800

    Scripts
    Replication works but no error handling yet between nodes.

---
+++ mysqlnode2.sql
@@ -0,0 +1,40 @@
+USE warehouse;
+SET sql_log_bin = OFF;
+CREATE USER IF NOT EXISTS 'rupert'@'%' IDENTIFIED WITH caching_sha2_password BY 'password';
+GRANT REPLICATION SLAVE ON *.* TO 'rupert'@'%';
+FLUSH PRIVILEGES;
+
+# Variable Sanity Check
+SHOW VARIABLES LIKE 'gtid_mode';
+SHOW VARIABLES LIKE 'log_bin';
+SHOW VARIABLES LIKE 'binlog_format';
+SHOW VARIABLES LIKE 'sql_log_bin';
+
+ALTER TABLE DimTitle AUTO_INCREMENT = 11923617;
+SET GLOBAL auto_increment_offset = 2;
+SET GLOBAL auto_increment_increment = 3;
+
+DELIMITER @@
+CREATE TRIGGER misc_create_trigger
+BEFORE INSERT ON DimTitle
+FOR EACH ROW
+BEGIN
+    IF NEW.titleType LIKE 'tv%' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = "Can not create new row for titleType TV in the MISC table";
+    END IF;
+END; @@
+DELIMITER ;
+
+CHANGE REPLICATION SOURCE TO SOURCE_HOST="stadvdb1.rinaldolee.com", SOURCE_USER="rupert", 
+SOURCE_PASSWORD="password", SOURCE_AUTO_POSITION=1, GET_SOURCE_PUBLIC_KEY=1 FOR CHANNEL "Node1ToNode2";
+
+CHANGE REPLICATION FILTER 
+    REPLICATE_DO_TABLE = (warehouse.DimTitle)
+FOR CHANNEL 'Node1ToNode2';
+
+START REPLICA FOR CHANNEL "Node1ToNode2";
+
+SHOW REPLICA STATUS FOR CHANNEL 'Node1ToNode2';
+
+SET sql_log_bin = ON;
---
+++ mysqlnode3.sql
@@ -0,0 +1,45 @@
+USE warehouse;
+SET sql_log_bin = OFF;
+CREATE USER IF NOT EXISTS 'rupert'@'%' IDENTIFIED WITH caching_sha2_password BY 'password';
+GRANT REPLICATION SLAVE ON *.* TO 'rupert'@'%';
+FLUSH PRIVILEGES;
+
+# Variable Sanity Check
+SHOW VARIABLES LIKE 'gtid_mode';
+SHOW VARIABLES LIKE 'log_bin';
+SHOW VARIABLES LIKE 'binlog_format';
+SHOW VARIABLES LIKE 'sql_log_bin';
+
+# Debugging
+SHOW MASTER STATUS;
+SHOW BINARY LOGS;
+SHOW BINLOG EVENTS IN 'mysql-bin.000001';
+
+ALTER TABLE DimTitle AUTO_INCREMENT = 11923617;
+SET GLOBAL auto_increment_offset = 3;
+SET GLOBAL auto_increment_increment = 3;
+
+DELIMITER @@
+CREATE TRIGGER tv_create_trigger
+BEFORE INSERT ON DimTitle
+FOR EACH ROW
+BEGIN
+    IF NEW.titleType NOT LIKE 'tv%' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = "Can not create new row for titleType MISC in the TV table";
+    END IF;
+END; @@
+DELIMITER ;
+
+CHANGE REPLICATION SOURCE TO SOURCE_HOST="stadvdb1.rinaldolee.com", SOURCE_USER="rupert", 
+SOURCE_PASSWORD="password", SOURCE_AUTO_POSITION=1, GET_SOURCE_PUBLIC_KEY=1 FOR CHANNEL "Node1ToNode3";
+
+CHANGE REPLICATION FILTER 
+    REPLICATE_DO_TABLE = (warehouse.DimTitle) 
+FOR CHANNEL 'Node1ToNode3';
+
+START REPLICA FOR CHANNEL "Node1ToNode3";
+
+SHOW REPLICA STATUS FOR CHANNEL "Node1ToNode3";
+
+SET sql_log_bin = ON;
