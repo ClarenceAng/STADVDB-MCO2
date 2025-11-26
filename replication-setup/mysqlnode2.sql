93efaf898367d6374eb2b013d94cfc09d1569205
Clarence Ang <clarence_ivan_ang@dlsu.edu.ph>
2025-11-26 21:28:31 +0800

    fixed folder and finally fixed replication

--- replication-setup/mysqlnode2.sql
+++ replication-setup/mysqlnode2.sql
@@ -1,4 +1,4 @@
-USE warehouse;
+USE nodedb;
 SET sql_log_bin = OFF;
 CREATE USER IF NOT EXISTS 'rupert'@'%' IDENTIFIED WITH caching_sha2_password BY 'password';
 GRANT REPLICATION SLAVE ON *.* TO 'rupert'@'%';
@@ -9,8 +9,9 @@ SHOW VARIABLES LIKE 'gtid_mode';
 SHOW VARIABLES LIKE 'log_bin';
 SHOW VARIABLES LIKE 'binlog_format';
 SHOW VARIABLES LIKE 'sql_log_bin';
+SHOW VARIABLES LIKE 'enforce_gtid_consistency';
 
-ALTER TABLE DimTitle AUTO_INCREMENT = 11923617;
+ALTER TABLE DimTitle AUTO_INCREMENT = 11923618;
 SET GLOBAL auto_increment_offset = 2;
 SET GLOBAL auto_increment_increment = 3;
 
@@ -26,11 +27,13 @@ BEGIN
 END; @@
 DELIMITER ;
 
+STOP REPLICA FOR CHANNEL "Node1ToNode2";
+
 CHANGE REPLICATION SOURCE TO SOURCE_HOST="stadvdb1.rinaldolee.com", SOURCE_USER="rupert", 
-SOURCE_PASSWORD="password", SOURCE_AUTO_POSITION=1, GET_SOURCE_PUBLIC_KEY=1 FOR CHANNEL "Node1ToNode2";
+SOURCE_PASSWORD="cliveisgay", SOURCE_PORT=3306, SOURCE_AUTO_POSITION=1, GET_SOURCE_PUBLIC_KEY=1 FOR CHANNEL "Node1ToNode2";
 
 CHANGE REPLICATION FILTER 
-    REPLICATE_DO_TABLE = (warehouse.DimTitle)
+    REPLICATE_DO_TABLE = (nodedb.DimTitle)
 FOR CHANNEL 'Node1ToNode2';
 
 START REPLICA FOR CHANNEL "Node1ToNode2";
@@ -38,3 +41,5 @@ START REPLICA FOR CHANNEL "Node1ToNode2";
 SHOW REPLICA STATUS FOR CHANNEL 'Node1ToNode2';
 
 SET sql_log_bin = ON;
+
+SET sql_log_bin = OFF;
--- replication-setup/mysqlnode3.sql
+++ replication-setup/mysqlnode3.sql
@@ -1,4 +1,4 @@
-USE warehouse;
+USE nodedb;
 SET sql_log_bin = OFF;
 CREATE USER IF NOT EXISTS 'rupert'@'%' IDENTIFIED WITH caching_sha2_password BY 'password';
 GRANT REPLICATION SLAVE ON *.* TO 'rupert'@'%';
@@ -9,13 +9,9 @@ SHOW VARIABLES LIKE 'gtid_mode';
 SHOW VARIABLES LIKE 'log_bin';
 SHOW VARIABLES LIKE 'binlog_format';
 SHOW VARIABLES LIKE 'sql_log_bin';
+SHOW VARIABLES LIKE 'enforce_gtid_consistency';
 
-# Debugging
-SHOW MASTER STATUS;
-SHOW BINARY LOGS;
-SHOW BINLOG EVENTS IN 'mysql-bin.000001';
-
-ALTER TABLE DimTitle AUTO_INCREMENT = 11923617;
+ALTER TABLE DimTitle AUTO_INCREMENT = 11923619;
 SET GLOBAL auto_increment_offset = 3;
 SET GLOBAL auto_increment_increment = 3;
 
@@ -31,11 +27,13 @@ BEGIN
 END; @@
 DELIMITER ;
 
+STOP REPLICA FOR CHANNEL "Node1ToNode3";
+
 CHANGE REPLICATION SOURCE TO SOURCE_HOST="stadvdb1.rinaldolee.com", SOURCE_USER="rupert", 
-SOURCE_PASSWORD="password", SOURCE_AUTO_POSITION=1, GET_SOURCE_PUBLIC_KEY=1 FOR CHANNEL "Node1ToNode3";
+SOURCE_PASSWORD="cliveisgay", SOURCE_PORT=3306, SOURCE_AUTO_POSITION=1, GET_SOURCE_PUBLIC_KEY=1 FOR CHANNEL "Node1ToNode3";
 
 CHANGE REPLICATION FILTER 
-    REPLICATE_DO_TABLE = (warehouse.DimTitle) 
+	REPLICATE_DO_TABLE = (nodedb.DimTitle)
 FOR CHANNEL 'Node1ToNode3';
 
 START REPLICA FOR CHANNEL "Node1ToNode3";
@@ -43,3 +41,5 @@ START REPLICA FOR CHANNEL "Node1ToNode3";
 SHOW REPLICA STATUS FOR CHANNEL "Node1ToNode3";
 
 SET sql_log_bin = ON;
+
+SET sql_log_bin = OFF;
